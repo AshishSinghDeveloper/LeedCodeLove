304309d23eb1bcfd004a34998376f205d8308f9d
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-08-06 23:24:56 -0500

    Solves Linked List Cycle II problem

---
+++ CorePlayground/LeedCodeL1/LInkedListCycleII.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using CorePlayground.LeedCodeL1;
+
+namespace LeedCodeLove.LeedCodeL1
+{
+    public static class LInkedListCycleII
+    {
+        public static ListNode DetectCycle(ListNode head)
+        {
+            HashSet<ListNode> listVal = new HashSet<ListNode>();
+            while (head != null && !listVal.Contains(head))
+            {
+                listVal.Add(head);
+                head = head.next;
+
+            }
+            return head;
+        }
+    }
+}
