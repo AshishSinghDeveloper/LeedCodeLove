ba9a12d9cb5dba77eb20e8b22ac624b8a9287e48
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-08-07 19:46:48 -0500

    Add solution for best time to buy and sell stock.

---
+++ CorePlayground/LeedCodeL1/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LeedCodeLove.LeedCodeL1
+{
+    public static class BestTimeToBuyAndSellStock
+    {
+        //TC: O(n) | SP: (1)
+        public static int MaxProfit(int[] prices)
+        {
+            int buy = 0;
+            int sell = 1;
+            int maxProfit = 0;
+            while (sell < prices.Length)
+            {
+                int profit = prices[sell] - prices[buy];
+                    if (profit < 0)
+                    {
+                        buy = sell;
+                    }
+                    else if (profit > maxProfit)
+                    {
+                        maxProfit = profit;
+                    }
+                sell++;
+            }
+            return maxProfit;
+        }
+    }
+}
