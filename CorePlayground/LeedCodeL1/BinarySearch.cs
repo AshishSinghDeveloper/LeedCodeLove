587c1a2eabab747d568e977a35bbd633143fe1d2
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-08-09 21:58:59 -0500

    Finds target in given ascending array.

---
+++ CorePlayground/LeedCodeL1/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿namespace LeedCodeLove.LeedCodeL1
+{
+    public static class BinarySearch
+    {
+		public static int Search(int[] nums, int target)
+		{
+			int start = 0;
+			int end = nums.Length - 1;
+			while (start <= end)
+			{
+				int middle = (start + end) / 2;
+				if (nums[middle] == target)
+				{
+					return middle;
+				}
+				else if (nums[middle] > target)
+				{
+					end = middle - 1;
+				}
+				else
+				{
+					start = middle + 1;
+				}
+			}
+			return -1;
+		}
+	}
+}
