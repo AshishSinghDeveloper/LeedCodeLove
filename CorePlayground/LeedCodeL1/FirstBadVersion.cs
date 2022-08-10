07cfb410274d12d8a760e64cefb0536ade918064
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-08-09 22:05:28 -0500

    Finds first bad product/version.

---
+++ CorePlayground/LeedCodeL1/FirstBadVersion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace LeedCodeLove.LeedCodeL1
+{
+    public static class FirstBadVersion
+    {
+		public static int VersionControl(int n)
+		{
+			if (n == 1) return 1;
+			int start = 1;
+			int end = n;
+			int answer = -1;
+
+			while (start < end)
+			{
+
+				int mid = start + (end - start) / 2; // (start + end) can be greateer than INT_MAX
+
+				if (IsBadVersion(mid)) 
+				{
+					end = mid;
+					answer = mid;
+				}
+				else
+				{
+					start = mid + 1;
+				}
+				if (start == end) return start;
+
+			}
+			return answer;
+
+		}
+
+		public static bool IsBadVersion(int n)
+        {
+			//This is an provided API call.
+			// returning true so that build do not fail
+			return true;
+        }
+	}
+}
