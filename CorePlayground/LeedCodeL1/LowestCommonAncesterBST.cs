dfbcc6b5bcc9eef61f748df4dc81948b77a0294c
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-08-10 22:25:23 -0500

    Submit LCA answer.

---
+++ CorePlayground/LeedCodeL1/LowestCommonAncesterBST.cs
@@ -0,0 +1,27 @@
+﻿namespace LeedCodeLove.LeedCodeL1
+{
+    public static class LowestCommonAncesterBST
+    {
+        public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            var current = root;
+            while (current != null)
+            {
+                if (p.val > current.val && q.val > current.val)
+                {
+                    current = current.right;
+                }
+                else if (p.val < current.val && q.val < current.val)
+                {
+                    current = current.left;
+                }
+                else
+                {
+                    return current;
+                }
+
+            }
+            return current;
+        }
+    }
+}
