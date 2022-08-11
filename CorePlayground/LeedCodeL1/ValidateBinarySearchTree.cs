84aa2d1aa414113df702181629f157fa3ebbea27
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-08-10 20:50:44 -0500

    solve Validate binary search tree

---
+++ CorePlayground/LeedCodeL1/ValidateBinarySearchTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeedCodeLove.LeedCodeL1
+{
+    public static class ValidateBinarySearchTree
+    {
+        public static bool IsValidBST(TreeNode root)
+        {
+            return IsValid(root, Decimal.MinValue, Decimal.MaxValue);
+        }
+
+        private static bool IsValid(TreeNode node, Decimal left, Decimal right)
+        {
+            if (node == null)
+                return true;
+            if (!(node.val < right && node.val > left))
+                return false;
+
+            return IsValid(node.left, left, node.val) && IsValid(node.right, node.val, right);
+        }
+    }
+}
