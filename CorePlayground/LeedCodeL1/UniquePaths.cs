e98b7275aec2d3ab40556d58614ab058e08c6ec6
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-08-13 22:29:04 -0500

    Solves unique path issue

---
+++ CorePlayground/LeedCodeL1/UniquePaths.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeedCodeLove.LeedCodeL1
+{
+    public static class UniquePaths
+    {
+        public static int UniquePath(int m, int n)
+        {
+            var memo = new Dictionary<string, int>();
+            return Recursion(m, n, memo);
+        }
+
+        private static int Recursion(int m, int n, Dictionary<string, int> memoization)
+        {
+            if (memoization.ContainsKey($"{m},{n}")) return memoization[$"{m},{n}"];
+            if (m == 1 && n == 1) return 1;
+            if (m == 0 || n == 0) return 0;
+
+            memoization.Add($"{m},{n}", Recursion(m - 1, n, memoization) + Recursion(m, n - 1, memoization));
+            return memoization[$"{m},{n}"];
+        }
+    }
+}
