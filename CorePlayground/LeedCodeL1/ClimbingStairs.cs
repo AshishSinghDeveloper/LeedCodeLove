9948bded7afef03d1bf5c2920f2afff1bac35802
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-08-13 22:28:01 -0500

    Submit Climbing stairs

---
+++ CorePlayground/LeedCodeL1/ClimbingStairs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeedCodeLove.LeedCodeL1
+{
+    public static class ClimbingStairs
+    {
+        public static int ClimbStairsAttempt1(int n)
+        {
+            int ans = Recursion(n);
+            return ans;
+        }
+
+        private static int Recursion(int n)
+        {
+            if (n < 2)
+                return 1;
+            return Recursion(n - 1) + Recursion(n - 2);
+        }
+
+        public static int ClimbStraisAttempt2(int n)
+        {
+            int one = 1;
+            int two = 1;
+            for (int i = 1; i <= n - 1; i++)
+            {
+                int temp = one;
+                one = one + two;
+                two = temp;
+            }
+            return one;
+        }
+    }
+
+    
+}
+
