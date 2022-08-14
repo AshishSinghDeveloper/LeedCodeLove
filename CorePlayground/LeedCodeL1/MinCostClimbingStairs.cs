a20dac6263c407a98ac697ba801020d87fb14760
AshishSinghDeveloper <ashishsng21@hotmail.com>
2022-08-13 22:28:45 -0500

    Submit min cost climbing stairs

---
+++ CorePlayground/LeedCodeL1/MinCostClimbingStairs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeedCodeLove.LeedCodeL1
+{
+    public static class MinCostClimbingStairs
+    {
+        public static int MinCostClimbingStair(int[] cost)
+        {
+            int first = cost[0];
+            int second = cost[1];
+
+            for (int i = 2; i < cost.Length; i++)
+            {
+                int min = Math.Min(first, second);
+                int temp = min + cost[i];
+                first = second;
+                second = temp;
+            }
+
+            return Math.Min(first, second);
+        }
+    }
+}
